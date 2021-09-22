63d3d8154ba375eaad4fb7c1f3d84d80b95637ce
williamsiewe16 <loicosiewe16@gmail.com>
2021-09-22 10:02:41 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Lab3-data-visualization---Streamlit
+Streamlit application for the visualization of two datasets : ny-trips-data.csv &amp; uber-raw-data-apr14.csv
---
+++ lab3 - Part 2 - SIEWE LOIC.py
@@ -0,0 +1,13 @@
+import matplotlib
+import streamlit as st
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import time
+
+
+st.components.v1.iframe("https://ip-api.com/", scrolling=True, height=400)
+
+st.components.v1.html("<body bgcolor='pink' style='display: flex; justify-content:center'><h1>My new component</h1></body>")
+
